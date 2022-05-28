23f11cc281ef345d82f8e805e3fe7910acdd5918
meetshahcode <72691054+meetshahcode@users.noreply.github.com>
2022-05-28 21:27:11 +0530

    model created

--- portfolio project/works/models.py
+++ portfolio project/works/models.py
@@ -1,4 +1,9 @@
+
 from django.db import models
 
 class works(models.Model):
-    pass
+    #image
+    image = models.ImageField(upload_to = 'images/')
+    #id
+    works_id = models.AutoField(primary_key=True)
+    summary = models.TextField()
