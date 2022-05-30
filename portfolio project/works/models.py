427411a68e8b20f60df1b06255407b300e815888
meetshahcode <72691054+meetshahcode@users.noreply.github.com>
2022-05-30 21:52:52 +0530

    Model finalized and update object string

--- portfolio project/works/models.py
+++ portfolio project/works/models.py
@@ -3,8 +3,22 @@ from django.db import models
 
 class work(models.Model):
     #image
-    image = models.ImageField(upload_to = 'images/')
+    Image = models.ImageField(upload_to = 'images/',null = True,blank=True)
     #id
-    work_id = models.AutoField(primary_key=True)
-    #summary
-    summary = models.TextField()
+    Work_id = models.AutoField(primary_key=True)
+    #title
+    Title = models.CharField(max_length=100,blank=False,unique=True,null=False)
+    #Detail
+    Detail = models.TextField(null=True)
+    #git link
+    Gitlink = models.URLField(blank=True)
+    #git boolean
+    Gitbool = models.BooleanField(default=False,blank=False,null=False)
+    #project website
+    Livelink = models.URLField(blank=True)
+    #project boolean
+    Livebool = models.BooleanField(default=False,blank=False,null=False)
+
+    def __str__(self):
+        return self.Title
+
