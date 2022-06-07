8c4183cf235588df38c461802740cbb515928d5b
meetshahcode <72691054+meetshahcode@users.noreply.github.com>
2022-06-07 14:46:35 +0530

    static file + image

--- portfolio project/portfolio/urls.py
+++ portfolio project/portfolio/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import works.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',works.views.Homepage,name = "Homepage")
-]
+
+] + static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
+"""
+py manage.py colleactstatic
+"""
